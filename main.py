f226fe9d6e81a0f431f02335bf2c357e69567636
Yash Pawar <yashpawarp@gmail.com>
2021-06-29 13:56:29 +0530

    🛠remove unnecessary comments in main.py

--- main.py
+++ main.py
@@ -15,11 +15,11 @@ init_time = datetime.datetime.now()
 
 users = {None:{'screentime':datetime.timedelta(0)}}
 
-
 # create object for all the people avaialable
 def add_user(name):
     users[name] = {'screentime': datetime.timedelta(0)}
 
+# add users in the users dictionary
 for name in known_faces.names:
     add_user(name)
 
@@ -38,55 +38,45 @@ def get_name(frame:np.array, face_loc:List[int]):
     return None
 
 
+def display_screen_time():
+    for user in users.keys():
+        if user is None: continue
+
+        print(f'{user}: {users[user]["screentime"]}')
+
+
 last_iter_people = set()  # to avoid NameError
 
 while True:
     try:
         success, frame = webcam.read()
 
-        # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # Converting the image to grey for better face recognition
-        # faces = face_detector.detectMultiScale(frame_gray, 1.3, 5)  # Detect for faces in the current frame
         faces = fr.face_locations(frame)
         num_faces = len(faces)
 
         print('Counting' if num_faces > 0 else 'Ideal 😴   ', end='\r')
 
-        # encoded_face = fr.face_encodings(frame, faces)[0]
-        # matches = fr.compare_faces(known_faces.encodings, encoded_face)
-        # distance = fr.face_distance(known_faces.encodings, encoded_face)
-        # idx = np.argmin(distance)
-
-        # if matches[idx]:
-        #     name = known_faces.names[idx]
-
-        # if num_faces >= 1 and not screen_time:
-        people = set()
+        # check for faces and start the timeer
+        people = set()  # to uniquely identify people
         for face_loc in faces:
             name = get_name(frame, face_loc)
             people.add(name)
+
             if not users[name].get('screen_time'):
                 users[name]['screen_time'] = True
                 users[name]['start_time'] = datetime.datetime.now()
-        # screen_time = True
-        # start_time = datetime.datetime.now()
-
+        
+        # check if some users left screen, if left then count the screentime
         for p in list(last_iter_people - people):
             # print(p)
-            users[p]['screen_time'] = False
-            users[p]['screentime'] += datetime.datetime.now() - users[p]['start_time']
+            if users[p]['screen_time']:
+                users[p]['screen_time'] = False
+                users[p]['screentime'] += datetime.datetime.now() - users[p]['start_time']
         
-        last_iter_people = people.copy()
-
-        # elif num_faces <= 0 and screen_time:
-
-        #     for face_loc in faces:
-        #         name = get_name(frame, face_loc)
-        #         users[name]['screen_time'] = False
-        #         users[name]['screen_up_time'] = datetime.datetime.now() - users[name]['start_time']
-            
-        #     # screen_time = False
-            # screen_up_time += datetime.datetime.now() - start_time
-            # print(screen_up_time)
+        last_iter_people = people.copy()  # helper to make the logic work
+        # all the people detected are stored in people:set
+        # the names of people is copied to last_iter_people:set
+        # in the next iteration we can calclate the missing people
 
         del frame
         time.sleep(1)
@@ -97,13 +87,20 @@ while True:
         print(users)
         print('Thank you for Using Face Screen Time')
         print('*-'*20)
+
+        # just a check before closing the programme
+        for p in list(last_iter_people - people):
+            if users[p]['screen_time']:
+                users[p]['screen_time'] = False
+                users[p]['screentime'] += datetime.datetime.now() - users[p]['start_time']
+
         webcam.release()
-        print(datetime.datetime.now() - init_time)
+       
         break
 
-    # except Exception as e:
-    #     print('some unknown error occured')
-    #     print(e)
-    #     break
+    except Exception as e:
+        print('some unknown error occured')
+        print(e)
+        break
 
 webcam.release()
